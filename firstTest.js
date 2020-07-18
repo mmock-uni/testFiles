a362414eda4ebee3aae7e05ed03e7fc2e0b33b28
Moritz Mock <mock.moritz@rolmail.net>
2020-07-18 21:00:46 +0200

    Update firstTest.js

--- firstTest.js
+++ firstTest.js
@@ -6,3 +6,13 @@ function x (value) {
       return 0;
   }
 }
+function x (value) {
+  switch(expression) {
+    case x>100:
+      return 1;
+    case x<50:
+      return 0;
+  }
+  default:
+    return 2;
+}
