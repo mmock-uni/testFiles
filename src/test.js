acaadbeb0a99bf7fefa38435b0e228b3c8318746
Moritz Mock <mock.moritz@rolmail.net>
2020-09-09 18:20:21 +0200

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,1 +1,54 @@
-test
+function x (value) {
+  switch(expression) {
+    case x>100:
+      return 1;
+    case x<50:
+      return 0;
+  }
+}
+function y (value) {
+  switch(expression) {
+    case x>100:
+      return 1;
+    case x<50:
+      return 0;
+  default:
+    return 2;
+  }
+}
+function x (value) {
+  switch(expression) {
+    case x>100:
+      return 1;
+    case x<50:
+      return 0;
+  }
+}
+function y (value) {
+  switch(expression) {
+    case x>100:
+      return 1;
+    case x<50:
+      return 0;
+  default:
+    return 2;
+  }
+}
+function x (value) {
+  switch(expression) {
+    case x>100:
+      return 1;
+    case x<50:
+      return 0;
+  }
+}
+function y (value) {
+  switch(expression) {
+    case x>100:
+      return 1;
+    case x<50:
+      return 0;
+  default:
+    return 2;
+  }
+}
