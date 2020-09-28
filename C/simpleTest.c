d9f90862e45ddd396154541d611444c19588c4cb
Moritz Mock <mock.moritz@rolmail.net>
2020-09-28 11:00:24 +0200

    changed C file

--- C/simpleTest.c
+++ C/simpleTest.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main()
+int a()
 {
     int a=2;
 
@@ -9,6 +9,35 @@ int main()
     	case 2: printf("a is two\n"); break;
     	case 3: printf("a is three\n"); break;
     	default: printf("a is something\n"); break;
+    }
+
+    return 0;
+}
+
+
+int b()
+{
+    int a=2;
+
+    switch(a) {
+    	case 1: printf("a is one\n"); 
+    	case 2: printf("a is two\n"); 
+    	case 3: printf("a is three\n"); 
+    	default: printf("a is something\n"); 
+    }
+
+    return 0;
+}
+
+
+int c()
+{
+    int a=2;
+
+    switch(a) {
+    	case 1: printf("a is one\n"); 
+    	case 2: printf("a is two\n"); 
+    	case 3: printf("a is three\n"); 
     }
 
     return 0;
