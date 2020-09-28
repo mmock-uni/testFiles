1889a7f492dd0079b53ccc66ef07552624fab4a6
Moritz Mock <mock.moritz@rolmail.net>
2020-09-28 09:55:21 +0200

    Create simpleTest.c

---
+++ C/simpleTest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=2;
+
+    switch(a) {
+    	case 1: printf("a is one\n"); break;
+    	case 2: printf("a is two\n"); break;
+    	case 3: printf("a is three\n"); break;
+    	default: printf("a is something\n"); break;
+    }
+
+    return 0;
+}
